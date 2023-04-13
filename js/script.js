575313964bda8eb54b5b6ff8a859792d10906541
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-04-13 14:00:04 +0200

    added wxtraction of single api to console

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 // get quotes from api
 let apiQuotes = [];
+
+// show new quote
+function newQuote(){
+    // pick random quotes from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+
 async function getQuotes(){
     const apiUrl = 'https://type.fit/api/quotes';
 
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        newQuote();
     } catch (error) {
         // errors here
     }
